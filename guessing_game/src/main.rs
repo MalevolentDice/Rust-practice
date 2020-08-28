f63fff92c2d07341768029f2da584a8fa9786e62
MalevolentDice <MalevolentDice@users.noreply.github.com>
2020-08-28 02:29:57 +0200

    guessing_game parse user input and compare to secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
@@ -14,6 +15,12 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {}", guess);
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => println!("You win!"),
+  }
 }
